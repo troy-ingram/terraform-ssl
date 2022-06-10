af34c1c123084d468ddee3072440b6a4bf87fc54
Troy Ingram <troyingram86@gmail.com>
2022-06-10 09:36:36 -0400

    update output name

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -1,5 +1,5 @@
 # --- dns/outputs.tf ---
 
 output "certificate_arn" {
-  value = aws_acm_certificate_validation.example.certificate_arn
+  value = aws_acm_certificate_validation.dns.certificate_arn
 }
